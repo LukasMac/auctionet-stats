b8fa07c6506d4c7d842a653d445a3f04da503f2d
Lukas Maciulis <lukas.maciulis@gmail.com>
2015-01-28 00:04:11 +0000

    Auctionet::Item#recent_bid_timestamp should return 0 if item has no bids

--- lib/auctionet/item.rb
+++ lib/auctionet/item.rb
@@ -5,6 +5,8 @@ module Auctionet
     end
 
     def recent_bid_timestamp
+      return 0 if @item["bids"].empty?
+
       recent_bid = @item["bids"].max_by { |bid| bid["timestamp"] }
       recent_bid["timestamp"]
     end
--- spec/auctionet/item_spec.rb
+++ spec/auctionet/item_spec.rb
@@ -2,25 +2,39 @@ require 'spec_helper'
 require 'auctionet/item'
 
 describe Auctionet::Item do
-  let(:item) do
-    Auctionet::Item.new({
+  let(:item_hash) do
+    {
       "id" => 1,
       "bids" => [
         { "timestamp" => 200 },
         { "timestamp" => 300 },
       ]
-    })
+    }
+  end
+  let(:item_with_no_bids_hash) do
+    {
+      "id" => 1,
+      "bids" => [ ],
+    }
+  end
+  let(:item) do
+    Auctionet::Item.new(item_hash)
   end
 
-  describe '#id' do
-    it 'should return correct item id' do
+  describe "#id" do
+    it "should return correct item id" do
       expect(item.id).to eq 1
     end
   end
 
-  describe '#recent_bid_timestamp' do
-    it 'should return most recent bid timestamp' do
+  describe "#recent_bid_timestamp" do
+    it "should return most recent bid timestamp" do
       expect(item.recent_bid_timestamp).to eq 300
+    end
+
+    it "should return 0 if item has no bids" do
+      item_with_no_bids = Auctionet::Item.new(item_with_no_bids_hash)
+      expect(item_with_no_bids.recent_bid_timestamp).to eq 0
     end
   end
 end
