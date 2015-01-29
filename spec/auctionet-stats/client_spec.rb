cdcd34554808aee3c93d81b35023f7d7fab61bbb
Lukas Maciulis <lukas.maciulis@gmail.com>
2015-01-29 10:19:22 +0000

    Refactor Auctionet::Client spec

--- spec/auctionet-stats/client_spec.rb
+++ spec/auctionet-stats/client_spec.rb
@@ -4,10 +4,12 @@ describe Auctionet::Client do
   describe ".items" do
     it "should return parsed JSON response" do
       allow(Auctionet::Client).to receive(:open) {
-        double('read', read: '{"items": [{"id": 1, "bids": [{"timestamp": 200}]}]}')
+        double("read", read: '{"items": [{"id": 1, "bids": [{"timestamp": 200}]}]}')
       }
 
-      parsed_response = [ { "id" => 1, "bids" => [ "timestamp" => 200 ] } ]
+      parsed_response = [
+        { "id" => 1, "bids" => [ "timestamp" => 200 ] },
+      ]
       expect(Auctionet::Client.items).to eq parsed_response
     end
 
